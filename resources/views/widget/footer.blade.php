6dc0c94e5672e3a947dca6c83cfbaeedada51927
huiyao351 <huiyao351@163.com>
2017-06-08 17:07:33 +0800

    upload resource

---
+++ resources/views/widget/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row text-center">
+            <ul>
+                <li><a href="http://www.gtmap.cn/" target="_blank">国图官网</a></li>
+                <li>&nbsp;·&nbsp;</li>
+                <li><a href="http://mail.gtmap.cn/" target="_blank">国图邮箱</a> </li>
+                <li>&nbsp;·&nbsp;</li>
+                <li><a href="http://oa.gtis.com.cn/" target="_blank">国图办公</a> </li>
+            </ul>
+
+        </div>
+    </div>
+</footer>
